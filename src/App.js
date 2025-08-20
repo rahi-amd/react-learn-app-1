b0a116655fb71e34da7939d1026a5c6facf23414
Kaynat <kaynat@Kaynats-MacBook-Air.local>
2025-08-20 14:35:30 +0530

    Initial React app setup - Khilauna Test App

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Khilauna Test App
         </a>
       </header>
     </div>
