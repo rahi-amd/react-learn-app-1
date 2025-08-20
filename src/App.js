2eb6329b73330eef3e712289d0bda617ac7d4640
Kaynat <kaynat@Kaynats-MacBook-Air.local>
2025-08-20 16:05:56 +0530

    Created StorySelector component - Phase 2 progress

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import StorySelector from './StorySelector';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>Testing Urdu Stories</h1>
+      <StorySelector />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the Urdu storytelling App!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Khilauna Test App
-        </a>
-      </header>
     </div>
   );
 }
---
+++ src/StorySelector.js
@@ -0,0 +1,9 @@
+function StorySelector(){
+    return (
+        <div className="story-selector">
+        <h2>Select a Story</h2>
+        <p>Story selection will go here</p>
+        </div>
+    );
+}
+export default StorySelector;
