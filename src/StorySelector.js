dd698ee66294cac41021e9f28761cab71b1f3c9c
Kaynat <kaynat@Kaynats-MacBook-Air.local>
2025-08-21 23:06:04 +0530

    Add shadows for button adn lion image

--- src/StorySelector.js
+++ src/StorySelector.js
@@ -1,9 +1,12 @@
 import './StorySelector.css';
+import skyBackground from './assets/images/sky-background-1.jpg';
+import lionTreasure from './assets/images/lion-treasure.png';
 
 
 function StorySelector(){
     return (
-        <div className="story-selector">
+        <div className="story-selector"
+            style={{ backgroundImage: `url(${skyBackground})` }}>
             <div className="story-card">
                 <div className="page-tabs">
             <div className="page-tab"></div>
@@ -18,18 +21,22 @@ function StorySelector(){
             <div className="page-tab"></div>
             </div>
                 <div className="story-illustration">
+                    <div className="image-area">
+                        <img src={lionTreasure} alt="Lion and Treasure" className="story-image" />
+
+          </div>
                     <div className="story-title">شیر اور خزانہ</div>
                 </div>
 
         <div className="story-options">
         <button className="read-to-me-btn">
-           🔊 مجھے پڑھ کر سنائیں</button>
+           🔊 مجھے  پڑھ  کر  سنائیں</button>
         <button className="read-it-myself-btn"> 
-              📖 مجھے خود پڑھنا ہے
+              📖 مجھے خود  پڑھنا  ہے
         </button>
-           </div>        
+           </div>  
+           </div>      
           </div>
-        </div>
     );
 }
 export default StorySelector;
