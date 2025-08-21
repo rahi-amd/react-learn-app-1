12616a44158e6cef08c89ffb74f9c9900ec89645
Kaynat <kaynat@Kaynats-MacBook-Air.local>
2025-08-21 10:48:02 +0530

    Add StoryReader component with basic structure

---
+++ src/StoryReader.js
@@ -0,0 +1,15 @@
+function StoryReader() {
+    return (
+        <div className="story-reader">
+        <h2>Story Title will Go Here</h2>
+        <div className="story-content">
+        <p>This is where the the urdu story conntent will appear!</p>
+        </div>
+        <div className="story-controls">
+        <button>Previous Page</button>
+        <button>Next Page</button>
+        </div>
+        </div>  
+    );    
+}
+export default StoryReader;
